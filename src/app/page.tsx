f152adc175f6aad689bf5f909c8d77942711460e
carlosaw <carlosfreevg@gmail.com>
2023-06-01 10:46:50 -0400

    Cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Olá NextJS</h1>
+      <div className="border-2 border-green-500 bg-vermelho text-vermelhoescuro">Olá NextJS</div>
     </div>
   );
 }
