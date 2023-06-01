d864876c4ad1462180ed5c9bb9e7236304074de1
carlosaw <carlosfreevg@gmail.com>
2023-06-01 16:55:08 -0400

    container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-const Page = () => {
-  return (
-    <div>
-      <div className="border-2 border-green-500 bg-vermelho text-vermelhoescuro">Olá NextJS</div>
+const Page = () => (
+  
+    <div className="container mx-auto bg-red-200">
+      
+      ...
+    
     </div>
-  );
-}
+  /*
+  NOME = width: 100%;
+  SM (small) = max-width: 640px;
+  MD (medium) = max-width: 768px;
+  LG (large) = max-width: 1024px;
+  XL (extra-large) = max-width: 1280px;
+  2Xl (extra-extra-large) = max-width: 1536px;
+  */
+)
 
 export default Page;
