b54b72a5a91cf071ae0406684cab56319246ba20
carlosaw <carlosfreevg@gmail.com>
2023-06-01 21:20:18 -0400

    Flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,97 @@
 const Page = () => (
   
-    <div className="container mx-auto bg-red-200">
+    <div className="container mx-auto">
+
+      <p className="flex justify-center mt-4">order-first</p>
+      <div className="flex flex-col gap-3 mx-3 sm:flex-row">
+        <div className="grow rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="order-first grow rounded-md bg-green-500 text-white p-4">02</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">order-last</p>
+      <div className="flex gap-3 mx-3">
+        <div className="grow rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="order-last grow rounded-md bg-green-500 text-white p-4">02</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
       
-      ...
-    
+      <p className="flex justify-center mt-4">flex-1</p>
+      <div className="flex">
+        <div className="flex-1 rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">grow</p>
+      <div className="flex gap-3 mx-3">
+        <div className="grow rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">02</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="grow rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">justify-around</p>
+      <div className="flex justify-around">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">justify-between</p>
+      <div className="flex justify-between">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">justify-center</p>
+      <div className="flex justify-center gap-4">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">items-center</p>
+      <div className="flex items-center justify-center">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-8">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-11">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">items-start</p>
+      <div className="flex items-start justify-center">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-8">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-11">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">items-end</p>
+      <div className="flex items-end justify-center">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-8">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-11">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+
+      <p className="flex justify-center mt-4">items-stretch</p>
+      <div className="flex items-stretch justify-center">
+        <div className="rounded-md bg-blue-500 text-white p-4">01</div>
+        <div className="rounded-md bg-blue-500 text-white p-8">02</div>
+        <div className="rounded-md bg-blue-500 text-white p-11">03</div>
+        <div className="rounded-md bg-blue-500 text-white p-4">04</div>
+      </div>
+   
     </div>
-  /*
-  NOME = width: 100%;
-  SM (small) = max-width: 640px;
-  MD (medium) = max-width: 768px;
-  LG (large) = max-width: 1024px;
-  XL (extra-large) = max-width: 1280px;
-  2Xl (extra-extra-large) = max-width: 1536px;
-  */
+  
 )
 
 export default Page;
