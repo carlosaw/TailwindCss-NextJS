0d9d58d5fa5d03f1f6623f7d9afbd520336e662c
carlosaw <carlosfreevg@gmail.com>
2023-06-14 16:33:06 -0400

    table-responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
 const Page = () => (
 
-  <div className="container mx-auto bg-white-500">
+  <div className="container mx-auto">
+    
+    <table className="w-full mx-2">
+      <thead>
+        <tr className="border-b text-left text-sm border-gray-500">
+          <th className="py-2">Nome</th>
+          <th className="hidden md:table-cell">Título</th>
+          <th className="hidden md:table-cell">Email</th>
+          <th>Função</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="border-b text-left text-sm border-gray-500">
+          <td className="py-2">
+            <p className="font-bold text-cyan-300">Maria da Silva</p>
+            <p className="block md:hidden text-gray-300">Cafezeira</p>
+            <p className="block md:hidden text-gray-300">mariadocafe@gmail.com</p>
+          </td>
+          <td className="hidden md:table-cell text-gray-300">Cafezeira</td>
+          <td className="hidden md:table-cell text-gray-300">mariadocafe@gmail.com</td>
+          <td>Membro</td>
+          <td>
+            <a href="">Editar</a>
+          </td>
+        </tr>
+        <tr className="border-b text-left text-sm border-gray-500">
+          <td className="py-2">
+            <p className="font-bold text-cyan-300">Gregório da Silva</p>
+            <p className="block md:hidden text-gray-300">Vigilante</p>
+            <p className="block md:hidden text-gray-300">vigia@gmail.com</p>
+          </td>
+          <td className="hidden md:table-cell text-gray-300">Vigilante</td>
+          <td className="hidden md:table-cell text-gray-300">vigia@gmail.com</td>
+          <td>Membro</td>
+          <td>
+            <a href="">Editar</a>
+          </td>
+        </tr>
+        <tr className="border-b text-left text-sm border-gray-500">
+          <td className="py-2">
+            <p className="font-bold text-cyan-300">Carlos da Silva</p>
+            <p className="block md:hidden text-gray-300">Gerente</p>
+            <p className="block md:hidden text-gray-300">carlos@gmail.com</p>
+          </td>
+          <td className="hidden md:table-cell text-gray-300">Gerente</td>
+          <td className="hidden md:table-cell text-gray-300">carlos@gmail.com</td>
+          <td>Membro</td>
+          <td>
+            <a href="">Editar</a>
+          </td>
+        </tr>
+        <tr className="border-b text-left text-sm border-gray-500">
+          <td className="py-2">
+            <p className="font-bold text-cyan-300">Fernando da Silva</p>
+            <p className="block md:hidden text-gray-300">Vendedor</p>
+            <p className="block md:hidden text-gray-300">fernando@gmail.com</p>
+          </td>
+          <td className="hidden md:table-cell text-gray-300">Vendedor</td>
+          <td className="hidden md:table-cell text-gray-300">fernando@gmail.com</td>
+          <td>Membro</td>
+          <td>
+            <a href="">Editar</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-<<<<<<< HEAD
-    <div className="p-3 rounded-md bg-gray-400 mt-8">
-      
-      <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="md:col-span-2 md:row-span-2 md:h-full border-2 border-cyan-800 rounded-md w-full h-60 mb-3 bg-cover bg-center bg-[url(https://www.smalldesignideas.com/wp-content/uploads/2015/02/stone-house-facade-design.jpg)]"></div>
-        <div className="md:ml-3 md:grid-cols-1 grid grid-cols-2 gap-3">
-          <div className="border-2 border-cyan-800 rounded-md w-full h-60 bg-cover bg-center bg-[url(https://images.adsttc.com/media/images/58f9/7c3c/e58e/cea0/5200/04e5/large_jpg/feature_image.jpg?1492745271)]"></div>
-          <div className="border-2 border-cyan-800 rounded-md w-full h-60 bg-cover bg-center bg-[url(https://s-media-cache-ak0.pinimg.com/originals/04/52/26/045226de010415e7db45df48848abfd7.jpg)]"></div>        
-        </div>
-      </div>
-      
-      <div className="text-2xl font-bold mt-3 text-cyan-800">Título da casa</div>
-=======
-  <div className="grid w-full bg-gray-800 p-4 gap-4 md:grid-cols-2">
-    <div className="
-      w-full 
-      h-60 
-      bg-cover 
-      bg-center
-      rounded-md
-      md:w-4/4
-      bg-[url(https://www.smalldesignideas.com/wp-content/uploads/2015/02/stone-house-facade-design.jpg)]">
-    </div>
-
-    <div className="grid grid-cols-2 w-full h-60 gap-4 md:grid-cols-1">
-      <div className="
-        w-full 
-        bg-cover 
-        bg-center
-        rounded-md
-        bg-[url(https://images.adsttc.com/media/images/58f9/7c3c/e58e/cea0/5200/04e5/large_jpg/feature_image.jpg?1492745271)]">
-      </div>
-
-      <div className="
-        bg-cover 
-        bg-center
-        rounded-md
-        bg-[url(https://s-media-cache-ak0.pinimg.com/originals/04/52/26/045226de010415e7db45df48848abfd7.jpg)]">
-      </div>
-    </div>
-
-  </div>
-
-  <div className="text-2xl font-bold mt-3">Título da casa</div>
-  <div className="flex gap-2 mt-3">
-    <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Campo</div>
-    <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Moderno</div>
-    <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Piscina</div>
-  </div>
-
-    {/* <div className="p-3 rounded-md bg-gray-400">
-      <div className="grid grid-cols-1 gap-16 md:grid-cols-3">
-        <div className="
-        w-full 
-        h-40 
-        bg-cover 
-        bg-center
-        rounded-md
-        md:col-span-2
-        md:row-span-2
-        md:h-full
-        bg-[url(https://www.smalldesignideas.com/wp-content/uploads/2015/02/stone-house-facade-design.jpg)]"></div>
-
-
-        <div className="
-        w-full 
-        h-40 
-        bg-cover 
-        bg-center
-        rounded-md 
-        bg-[url(https://images.adsttc.com/media/images/58f9/7c3c/e58e/cea0/5200/04e5/large_jpg/feature_image.jpg?1492745271)]"></div>
-
-        <div className="
-        w-full 
-        h-40 
-        bg-cover 
-        bg-center
-        rounded-md
-        bg-[url(https://s-media-cache-ak0.pinimg.com/originals/04/52/26/045226de010415e7db45df48848abfd7.jpg)]"></div>
-      </div>
-      <div className="text-2xl font-bold mt-3">Título da casa</div>
->>>>>>> 3a4bd48bb22ca94d967a5a93aca2930076d08c28
-      <div className="flex gap-2 mt-3">
-        <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Campo</div>
-        <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Moderno</div>
-        <div className="rounded-full px-3 py-1 text-sm bg-green-700 text-white">Piscina</div>
-      </div>
-<<<<<<< HEAD
-
-    </div>
-
-=======
-    </div> */}
->>>>>>> 3a4bd48bb22ca94d967a5a93aca2930076d08c28
   </div>
 
 );
